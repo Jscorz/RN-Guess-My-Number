2ed51e23945d4006738c9e62aafa391f56e4c096
Justin Scorzafava <jkscorzafava@gmail.com>
2022-12-14 20:29:16 -0700

    added userNumber, roundsNumber, and startNewGameHandler as props to gameoverscreen so dynamic data could be used and game reset

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import Colors from "./constants/colors";
 export default function App() {
 	const [userNumber, setUserNumber] = useState();
 	const [gameIsOver, setGameIsOver] = useState(true);
+	const [guessRounds, setGuessRounds] = useState(0);
 
 	const [fontsLoaded] = useFonts({
 		"open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
@@ -38,7 +39,18 @@ export default function App() {
 	}
 
 	if (gameIsOver && userNumber) {
-		screen = <GameOverScreen />;
+		screen = (
+			<GameOverScreen
+				userNumber={userNumber}
+				roundsNumber={guessRounds}
+				onStartNewGame={startNewGameHandler}
+			/>
+		);
+	}
+
+	function startNewGameHandler() {
+		setUserNumber(null);
+		setGuessRounds(0);
 	}
 
 	return (
