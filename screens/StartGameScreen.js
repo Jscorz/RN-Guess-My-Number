94c22e62853ed0050494c19ec01550793c22fa6c
Justin Scorzafava <jkscorzafava@gmail.com>
2022-12-16 20:14:56 -0700

    updated marginTop using dynamic sizing with Dimensions API calculating width and height inside of component function that way it rerenders/recalculates when switching between landscape/portrait modes

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, TextInput, View, Alert } from "react-native";
+import { StyleSheet, TextInput, View, Alert, useWindowDimensions } from "react-native";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Colors from "../constants/colors";
 import Title from "../components/ui/Title";
@@ -8,6 +8,8 @@ import InstructionText from "../components/ui/InstructionText";
 
 function StartGameScreen({ onPickNumber }) {
 	const [enteredNumber, setEnteredNumber] = useState("");
+
+	const { width, height } = useWindowDimensions();
 
 	function numberInputHandler(enteredText) {
 		setEnteredNumber(enteredText);
@@ -29,8 +31,10 @@ function StartGameScreen({ onPickNumber }) {
 		onPickNumber(chosenNumber);
 	}
 
+	const marginTopDistance = height < 380 ? 30 : 100;
+
 	return (
-		<View style={styles.rootContainer}>
+		<View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
 			<Title>Guess My Number</Title>
 			<Card>
 				<InstructionText>Enter a Number</InstructionText>
@@ -58,10 +62,12 @@ function StartGameScreen({ onPickNumber }) {
 
 export default StartGameScreen;
 
+// const deviceHeight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
 	rootContainer: {
 		flex: 1,
-		marginTop: 100,
+		// marginTop: deviceHeight < 380 ? 30 : 100,
 		alignItems: "center",
 	},
 	numberInput: {
